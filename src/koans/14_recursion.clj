dfdbc097cfdb18a557542b7b643d896fc9ebab42
Tom Orth <torth212@gmail.com>
2018-08-21 17:31:47 -0400

    Add 7-14 koans

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,27 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
-  (loop [n   n
+  (loop [n  n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [in coll out '()] 
+    (if(empty? in)
+      out
+      (recur (rest in) (cons (first in) out)))))
 
 (defn factorial [n]
-  __)
+  (loop [n n acc 1]
+    (if (= 0 n)
+      acc
+      (recur (dec n) (* n acc)))))
 
 (meditations
   "Recursion ends with a base case"
